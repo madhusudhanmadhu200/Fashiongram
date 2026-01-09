4e6b6f80aafde2742863c5660a5356541861eb65
madhusudhanmadhu200 <madhusudhanmadhu.200@gmail.com>
2026-01-09 23:28:13 +0530

    Changed from Gmail to Resend

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,22 +1,14 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport( {
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // STARTTLS
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-} );
+const resend = new Resend( process.env.RESEND_API_KEY );
 
-export default async function sendEmail( to, subject, text ) {
-    await transporter.sendMail( {
-        from: process.env.EMAIL_USER,
+const sendEmail = async ( to, subject, text ) => {
+    await resend.emails.send( {
+        from: "Fashiongram <onboarding@resend.dev>",
         to,
         subject,
         text,
     } );
-}
+};
+
+export default sendEmail;
