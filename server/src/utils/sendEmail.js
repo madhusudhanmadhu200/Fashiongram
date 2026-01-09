7c8d1a7c34104bc6487edc9fa56caad7cf39cabf
madhusudhanmadhu200 <madhusudhanmadhu.200@gmail.com>
2026-01-09 23:15:39 +0530

    Changing nodemailer

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,19 +1,22 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport( {
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // STARTTLS
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
 } );
-const sendEmail = async ( to, subject, text ) => {
+
+export default async function sendEmail( to, subject, text ) {
     await transporter.sendMail( {
-        from: process.env.EMAIL_USER, // IMPORTANT: keep simple
+        from: process.env.EMAIL_USER,
         to,
         subject,
         text,
     } );
-};
-
-export default sendEmail;
+}
